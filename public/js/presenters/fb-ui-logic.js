46bd03df593aacefadbc1c23673c2ae316a8ef6c
Fernando Catacora <nando.drw@gmail.com>
2014-07-02 15:47:02 -0500

    add templates

--- public/js/presenters/fb-ui-logic.js
+++ public/js/presenters/fb-ui-logic.js
@@ -2,26 +2,34 @@
 
   var UI = function() {
 
-    function handlePhotos(photos) {
+    function handlePhotos(photos,type) {
       $('#photo-grid').empty();
       for(var i = 0; i < photos.length; i++) {
-        displayPhoto(photos[i]);
+        eval('displayPhoto_' + type)(photos[i]);
+        // displayPhoto_likes(photos[i]);
       }
     }
 
-    function displayPhoto(photo) {
-      var template = $('#templates .tmp-photo').html();
+    function displayPhoto_likes(photo) {
+      var template = $('#templates .tmp-photo-like').html();
+      Mustache.parse(template);
+      var rendered = Mustache.render(template, photo);
+      $('#photo-grid').append(rendered);
+    }
+
+    function displayPhoto_comments(photo) {
+      var template = $('#templates .tmp-photo-comment').html();
       Mustache.parse(template);
       var rendered = Mustache.render(template, photo);
       $('#photo-grid').append(rendered);
     }
 
     function renderPhotosByLikes() {
-      handlePhotos( bl.sortPhotosByLikes() );
+      handlePhotos( bl.sortPhotosByLikes(),'likes' );
     }
 
     function renderPhotosByComments() {
-      handlePhotos( bl.sortPhotosByComments() );
+      handlePhotos( bl.sortPhotosByComments(),'comments' );
     }
 
     $(document).on('click', '#likes', function(e) {
