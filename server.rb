6a5c1d9d089d7799fc1d8a9c84ab97c58ee1e5ea
Fernando Catacora <nando.drw@gmail.com>
2014-07-02 13:33:12 -0500

    clean files

--- server.rb
+++ server.rb
@@ -14,36 +14,3 @@ get '/' do
 	erb :login
 end
 
-get '/filter' do
-  erb :filter
-end
-
-# get '/' do
-#   result = RPS::ValidateSession.run(session)
-#   if result[:success?]
-#     @errors = result[:errors]
-#     erb :something
-#   else
-#     erb :index
-#   end
-# end
-
-
-#-------- JSON API routes -----------
-
-# post '/api/players/:player_id/matches/:match_id/games/:game_id' do |player_id,match_id,game_id|
-#   result = RPS::ValidateSession.run(session)
-#   @errors = result[:errors]
-#   @player = result[:player]
-
-#   if result[:success?]
-#     play_hash = {:player => @player, :match_id => match_id, :game_id => game_id, :action => params[:action]}
-#     result = RPS::Play.run(play_hash)
-
-#     result[:errors].push(@errors).flatten!
-#   else
-#     result = {:errors => @errors}
-#   end
-
-#   JSON(result)
-# end
